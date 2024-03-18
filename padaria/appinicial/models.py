23b4cbc4c97d38e3d025d42a78371e3d016d1d57
lucasdev <blesslucas89@gmail.com>
2024-03-18 18:16:57 -0500

    migrações feitas

--- padaria/appinicial/models.py
+++ padaria/appinicial/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class contato(models.model):
+class contato(models.Model):
     nome = models.CharField(verbose_name='Nome', max_length=50)
-    email = models.emailField(verbose_name = 'E-mail', max_length=75)
+    email = models.EmailField(verbose_name = 'E-mail', max_length=75)
     mensagem = models.TextField(verbose_name = 'Mensagem', max_length=100)
-    data =  models.DateTimeFiled(verbose_name = 'data de envio', auto_now_add = True)
+    data =  models.DateTimeField(verbose_name = 'data de envio', auto_now_add = True)
     lido = models.BooleanField(verbose_name ='Lido', default=False, blank=True)
     
     def __str__(self):
@@ -13,6 +13,6 @@ class contato(models.model):
     
     class Meta:
         verbose_name = 'Formulario de Contato'
-        verbose_plural =  'Formularios de Contatos'
+        verbose_name_plural =  'Formularios de Contatos'
         
     
