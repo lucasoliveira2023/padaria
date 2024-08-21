a4ac156faedd40038e4d4bd5f485fdb93f509c0a
lucasoliveira23 <blesslucas89@gmail.com>
2024-08-21 11:08:30 -0500

    urls raiz config

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from financeiro import views
-from .views import  TransacaoListCreate
+#from .views import  TransacaoListCreate
 
 urlpatterns = [
     path('/transacoes/', views.list_transacao, name='lista_transacoes'),
-    path('/transacoes/', TransacaoListCreate.as_views(), name='transacao_list_create'),
+    #path('/transacoes/', TransacaoListCreate.as_views(), name='transacao_list_create'),
 ]
--- padaria/urls.py
+++ padaria/urls.py
@@ -17,14 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from appinicial.views import inicio, index, Contato
-from financeiro import views
-
+from financeiro.views import create_transaction, update_transaction, delete_transaction, list_transacao
+from . import views
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('transacoes/', views.list_transacao, name='lista_transacoes'),
+    path('transacoes/', list_transacao, name='lista_transacoes'),
     #path('', include('myapp.urls')),
     path('pgcontato/', Contato, name='contato'),
     path('admin/', admin.site.urls),
+    
+    path('create/', create_transaction, name='transaction_create'),
+    path('update/<int:pk>/', update_transaction, name='transaction_update'),
+    path('delete/<int:pk>/', delete_transaction, name='transaction_delete'),
     
 ]
