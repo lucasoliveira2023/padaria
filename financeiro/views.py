b83ad140e77433de97ad8681a36e1c8e3db5059c
lucasoliveira23 <blesslucas89@gmail.com>
2024-08-21 10:57:45 -0500

    Crud views

--- financeiro/views.py
+++ financeiro/views.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import generics
 # Create your views here.
 from financeiro.models import Transacao
 from financeiro.serializers import TransacaoSerializer
+from .forms import TransacaoForm
 
 def list_transacao(request):
     transacoes = Transacao.objects.all()
     return render(request, 'lista_transacoes.html', {'transacoes': transacoes})
 
 
-class TransacaoListCreate(generics.ListCreateAPIView):
-    queryset = Transacao.objects.all()
-    serializer_class = TransacaoSerializer
+#class TransacaoListCreate(generics.ListCreateAPIView):
+#    queryset = Transacao.objects.all()
+#    serializer_class = TransacaoSerializer
+    
+    
+def create_transaction(request):
+    if request.method == 'POST':
+        form = TransacaoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_transacoes')
+        
+        else:
+            form = TransacaoForm()
+            
+        return render(request, 'lista_transacoes.html', {'form': form})
+                      
+
+def update_transaction(request, pk):
+    transaction = get_object_or_404(Transacao, pk=pk)
+    if request.method == 'UPDATE':
+        form = TransacaoForm(request.UPDATE, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransacaoForm(instance=transaction)
+    return render(request, 'lista_transacoes.html', {'form':form})
+
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transacao, pk=pk)
+    if request.method == 'DELETE':
+        transaction.delete()
+        return redirect('transaction_list')
+    return render(request, 'finance/transaction_confirm_delete.html', {'transaction':transaction})
